f7cd08e3d824f36f74cba3429b5cfc0dc35c15a6
junoha <jun.ohashi.42@gmail.com>
2021-02-13 23:48:19 +0900

    Add comment & refactor

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,26 +4,27 @@ import * as cdk from '@aws-cdk/core';
 import { DynamoDbHistoryRecorderStack, DynamoDbHistoryRecorderStackProps } from '../lib/ddb-history-recorder';
 import { Tags } from '@aws-cdk/core';
 
+/**
+ * Validate and get parameter
+ * @param app App
+ */
 function validateAndGetParameter(app: cdk.App) {
   const bucket: string = app.node.tryGetContext('s3-bucket') ?? process.env.S3_BUCKET;
-  if (!bucket) {
-    throw new Error('s3-bucket must be set. -c s3-bucket=xxx or S3_BUCKET=xxx');
-  }
+  if (!bucket) throw new Error('s3-bucket must be set. -c s3-bucket=xxx or S3_BUCKET=xxx');
 
   const prefix: string = app.node.tryGetContext('s3-prefix') ?? process.env.S3_PREFIX;
-  if (!prefix) {
-    throw new Error('s3-prefix must be set. -c s3-prefix=xxx or S3_PREFIX=xxx');
-  }
+  if (!prefix) throw new Error('s3-prefix must be set. -c s3-prefix=xxx or S3_PREFIX=xxx');
 
   const hive: string = app.node.tryGetContext('use-hive-partition') ?? process.env.USE_HIVE_PARTITION;
-  if (hive && !(/true|false/.test(hive))) {
-    throw new Error('use-hive-partition should be true or false');
-  }
+  if (hive && !(/true|false/.test(hive))) throw new Error('use-hive-partition should be true or false');
   const hivePartition = hive && hive === 'true' ? true : false;
 
   return { bucket, prefix, hivePartition }
 }
 
+/**
+ * Entry point
+ */
 function main() {
   const app = new cdk.App();
   const param = validateAndGetParameter(app);
